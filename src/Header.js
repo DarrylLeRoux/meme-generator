da1e2526e5018a11281b5ef0e557c3991a3e38a7
Darryl Le Roux <devdarryl@outlook.com>
2022-08-11 21:53:20 +0200

    Used instructors css

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,12 @@
 import React from "react";
-import logo from "./images/troll-face.png";
+import troll from "./images/troll-face.png";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">
-        <img src={logo} alt="meme face" />
-        <p>Meme Generator</p>
-      </div>
-      <div className="course">
-        <p>React Course - Project 3</p>
-      </div>
+      <img src={troll} alt="meme face" className="header--image" />
+      <h3 className="header--title">Meme Generator</h3>
+      <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
 };
