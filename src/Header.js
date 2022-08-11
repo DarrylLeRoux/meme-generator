30255651cdaad15aed26fd5a26528256b94e1128
Darryl Le Roux <devdarryl@outlook.com>
2022-08-11 21:38:26 +0200

    Added full header

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,13 @@ import logo from "./images/troll-face.png";
 
 const Header = () => {
   return (
-    <header>
+    <header className="header">
       <div className="logo">
         <img src={logo} alt="meme face" />
         <p>Meme Generator</p>
+      </div>
+      <div className="course">
+        <p>React Course - Project 3</p>
       </div>
     </header>
   );
