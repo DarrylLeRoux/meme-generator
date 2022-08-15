68be4734f775a181c8a755c256fbfdf828ee37df
Darryl Le Roux <devdarryl@outlook.com>
2022-08-15 21:34:05 +0200

    Added form button click event

--- src/Meme.js
+++ src/Meme.js
@@ -1,12 +1,22 @@
 import React from "react";
+import memesData from "./memesData";
 
 const Meme = () => {
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+  }
+
   return (
     <main>
       <form className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Generate Meme</button>
+        <button className="form--button" onClick={getMemeImage}>
+          Generate Meme
+        </button>
       </form>
     </main>
   );
